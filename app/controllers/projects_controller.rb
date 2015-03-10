bfe68ff49511749d588828c9e057d6a72d769285
Swastik <swastik@aceteq.co>
2015-03-10 20:48:10 +0530

    Changes

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,12 +3,15 @@ class ProjectsController < ApplicationController
   def index
     @projects = current_user.projects
   end
+
   def show
     @project=Project.find(params[:id])
   end
+
   def new
     @project=Project.new
   end
+
   def create
     @project = Project.new(project_params)
     if @project.save
@@ -18,18 +21,27 @@ class ProjectsController < ApplicationController
       render json: @project.errors, status: 422
     end
   end
+
   def destroy
     @project=Project.find(params[:id])
     @project.destroy
     render json: @project, status: 200
   end
+
   def update
-    @project=Project.find(params[:id])
-    @project.update_attributes(project_params)
-    users = params[:users].split(",")
-    users.each do |user|
-    @project.users << user
+    @project = Project.find(params[:id])
+
+    users = params[:project][:users].split(",")
+
+    users.each do |username|
+      user = User.find_by username: username
+      @project.users << user unless user.nil?
     end
+
+    modified_params = project_params.except(:users)
+
+    @project.update_attributes(modified_params)
+
     render json: @project, status: 200
   end
 
@@ -37,5 +49,5 @@ class ProjectsController < ApplicationController
       def project_params
         params.require(:project).permit(:name, :id, :users, :created_by_id)
       end
-      
+
 end
