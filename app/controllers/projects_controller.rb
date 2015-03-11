57fffa9385622a47af205c20e40b264296a80d9f
Swastik <swastik@aceteq.co>
2015-03-11 20:27:00 +0530

    API compat

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,15 +1,14 @@
 class ProjectsController < ApplicationController
-  before_filter :authenticate_user!
+  # before_filter :authenticate_user!
+
   def index
     @projects = current_user.projects
+    render json: { projects: @projects }, status: 200
   end
 
   def show
-    @project=Project.find(params[:id])
-  end
-
-  def new
-    @project=Project.new
+    @project = Project.find(params[:id])
+    render json: { project: @project }, status: 200
   end
 
   def create
@@ -23,14 +22,13 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project=Project.find(params[:id])
+    @project = Project.find(params[:id])
     @project.destroy
     render json: @project, status: 200
   end
 
   def update
-    @project = Project.find(params[:id])
-
+    @project  =  Project.find(params[:id])
     @project.update_attributes(project_params)
 
     render json: @project, status: 200
@@ -38,7 +36,7 @@ class ProjectsController < ApplicationController
 
     private
       def project_params
-        params.require(:project).permit(:name, :id, :created_by_id, :user_ids => [])
+        params.require(:project).permit(:name, :id, :created_by_id, :user_ids  => [])
       end
 
 end
