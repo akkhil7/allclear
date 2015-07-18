fcc476315f92639f5630ac1c8351f681ecb280a7
Akhil Ramesh <akkhil@outlook.com>
2015-07-19 02:34:43 +0530

    added comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  def index
+    @comments = Comment.all
+    render json: @comments, status: 200
+  end
+
+  def show
+    @comment = Comment.find(params[:id])
+    render json: @comment, status: 200
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save!
+      render json: @comment, status: 200
+    else
+      render json: @comment.errors, status: 422
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:id, :body, :issue_id, :user_id)
+  end
+
 end
